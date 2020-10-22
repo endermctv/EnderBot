c41e768e45d0eb223bb3588ea4da5ed0e9d5fd40
EnderMC <55290516+endermctv@users.noreply.github.com>
2020-10-22 16:48:08 +0200

    Mofification de quelques CSS et du serveur

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import * as express from 'express'
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => console.log(`Le serveur est démarré à l'adresse http://localhost:${PORT}.`))
+app.listen(PORT, () => console.log(`Le serveur est démarré à l'adresse ${location.pathname}:${PORT}.`))
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`)
